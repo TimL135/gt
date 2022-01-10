7d1e646a3d3cd7493a34df38d74269a70d145876
Dominic.Blankenfeldt <d.blankenfeldt@mbd-team.de>
2022-01-10 16:57:12 +0100

    creating random enemies and movement of the enemies

--- src/components/JumpRun.vue
+++ src/components/JumpRun.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="game" tabindex="0">
     <div id="char" :class="{}"></div>
-    <div :class="{}"></div>
-    <div :class="{}"></div>
+    <div
+      :id="Enemy.id"
+      :class="Enemy.size"
+      v-for="Enemy of Enemies"
+      :key="Enemy"
+    ></div>
   </div>
   <button id="btn" @click="start()" :class="{ startBtn: true == gameStarted }">
     Start Game
@@ -12,6 +16,13 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+interface Enemy {
+  x: number;
+  y: number;
+  size: string;
+  id: string;
+  moveVektor: number[];
+}
 
 export default defineComponent({
   data() {
@@ -21,6 +32,13 @@ export default defineComponent({
       y: 104,
       positionvar: 0,
       pressedKeys: {} as Record<string, boolean>,
+      Enemies: [] as Enemy[],
+      // Enemy: {
+      //   x: 0,
+      //   y: 0,
+      //   size: "eSmall",
+      //id:""
+      // },
     };
   },
   computed: {
@@ -31,6 +49,7 @@ export default defineComponent({
   methods: {
     gameloop() {
       this.handlePlayerMovement();
+      this.handleEnemyMovement();
     },
     handlePlayerMovement() {
       if (this.pressedKeys["ArrowDown"]) {
@@ -46,7 +65,65 @@ export default defineComponent({
         this.up();
       }
     },
-    createEnemy() {},
+    handleEnemyMovement() {
+      for (let enemy of this.Enemies) {
+        enemy.x += enemy.moveVektor[0];
+        enemy.y += enemy.moveVektor[1];
+        let enemyDiv = document.getElementById(enemy.id);
+        enemyDiv!.style.position = "absolute";
+        enemyDiv!.style.left = enemy.x + "px";
+        enemyDiv!.style.top = enemy.y + "px";
+      }
+    },
+    createEnemy() {
+      let size = "";
+      let x = 0;
+      let y = 0;
+      let moveArray = [] as number[];
+      switch (this.getRandomInt(4)) {
+        case 0:
+          y = 105 - 25;
+          moveArray = [(Math.random() - 0.5) * 2, 1];
+          break;
+        case 1:
+          y = 638 + 25;
+          moveArray = [(Math.random() - 0.5) * 2, -1];
+          break;
+        case 2:
+          x = 790 + 25;
+          moveArray = [-1, (Math.random() - 0.5) * 2];
+          break;
+        case 3:
+          x = 255 - 25;
+          moveArray = [1, (Math.random() - 0.5) * 2];
+          break;
+      }
+
+      if (!x) {
+        x = this.getRandomInt(534);
+      }
+      if (!y) {
+        y = this.getRandomInt(534);
+      }
+      switch (this.getRandomInt(3)) {
+        case 0:
+          size = "eSmall";
+          break;
+        case 1:
+          size = "eMedium";
+          break;
+        case 2:
+          size = "eBig";
+          break;
+      }
+      this.Enemies[this.Enemies.length] = {
+        x: x,
+        y: y,
+        size: size,
+        id: JSON.stringify(this.getRandomInt(100000000)),
+        moveVektor: moveArray,
+      };
+    },
     getRandomInt(max: number) {
       return Math.floor(Math.random() * max);
     },
@@ -61,7 +138,7 @@ export default defineComponent({
       setInterval(() => {
         this.gameloop();
       }, 1000 / 60);
-
+      this.createEnemy();
       console.log("game started");
     },
     up() {
@@ -131,17 +208,17 @@ export default defineComponent({
   height: 15px;
   background-color: red;
 }
-#enemySmall {
+.eSmall {
   width: 15px;
   height: 15px;
   background-color: rgb(99, 206, 50);
 }
-#enemyMedium {
+.eMedium {
   width: 20px;
   height: 20px;
   background-color: rgb(50, 206, 198);
 }
-#enemyBig {
+.eBig {
   width: 25px;
   height: 25px;
   background-color: rgb(84, 50, 206);

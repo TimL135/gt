9ea7e986538af20accd0167feaea723e492ddb13
tim <t.littschwager@mbd-team.de>
2022-01-14 12:15:14 +0100

    items; curve;colorchange

--- src/components/JumpRun.vue
+++ src/components/JumpRun.vue
@@ -14,14 +14,27 @@
       :class="Enemy.size"
       v-for="Enemy of Enemies"
       :key="Enemy"
-      :style="{ left: Enemy.x + 'px', top: Enemy.y + 'px' }"
+      :style="{
+        left: Enemy.x + 'px',
+        top: Enemy.y + 'px',
+        backgroundColor: Enemy.color,
+      }"
+    ></div>
+    <div
+      :class="item.type"
+      v-for="item of items"
+      :key="item"
+      :style="{
+        left: item.cords[0] + 'px',
+        top: item.cords[1] + 'px',
+      }"
     ></div>
 
     <button
       id="btn"
       @click="start()"
       :class="{ startBtn: true == gameStarted }"
-      class="btn btn-success align-self-center"
+      class="btn btn-success align-self-center shadow-none"
     >
       Start Game
     </button>
@@ -35,12 +48,20 @@ interface Enemy {
   y: number;
   size: string;
   id: string;
+  type: string;
+  color: string;
   moveVektor: number[];
 }
 interface Sizes {
   eSmall: 15;
   eMedium: 20;
   eBig: 25;
+}
+interface Item {
+  type: string;
+  cords: number[];
+  size: string;
+  timer: number;
 }
 export default defineComponent({
   data() {
@@ -57,6 +78,7 @@ export default defineComponent({
       y: 0,
       gameloopCounter: 0,
       positionvar: 0,
+      items: [] as Item[],
       pressedKeys: {} as Record<string, boolean>,
       Enemies: [] as Enemy[],
     };
@@ -86,9 +108,12 @@ export default defineComponent({
       this.handleEnemyMovement();
       this.score += this.difficulty;
       this.colisionHandling();
+      this.despawnItems();
       this.gameloopCounter++;
-      this.gameloopCounter % 1200 == 0 ? (this.difficulty += 0.5) : null;
-      this.gameloopCounter % 1800 == 0 ? this.createEnemy() : null;
+      this.gameloopCounter % 60 == 0 ? this.handleEnemyColorSwitch() : null; //1sek
+      this.gameloopCounter % 420 == 0 ? this.spawnItems() : null; //7sek
+      this.gameloopCounter % 1200 == 0 ? (this.difficulty += 0.5) : null; //20sek
+      this.gameloopCounter % 1800 == 0 ? this.createEnemy() : null; //30sek
     },
     colisionHandling() {
       let map = {
@@ -96,6 +121,33 @@ export default defineComponent({
         eMedium: 20,
         eBig: 25,
       };
+      for (let item of this.items) {
+        if (
+          Math.sqrt(
+            (item.cords[0] +
+              map[item.size as keyof Sizes] / 2 -
+              (this.x + 7.5)) **
+              2 +
+              (item.cords[1] +
+                map[item.size as keyof Sizes] / 2 -
+                (this.y + 7.5)) **
+                2
+          ) <
+          map[item.size as keyof Sizes] / 2 + 7.5
+        ) {
+          switch (item.type) {
+            case "coin":
+              this.items.splice(
+                this.items.findIndex((i) => i == item),
+                1
+              );
+              this.collectCoin();
+              break;
+            case "bomb":
+              break;
+          }
+        }
+      }
       for (let enemy of this.Enemies) {
         if (
           Math.sqrt(
@@ -111,6 +163,45 @@ export default defineComponent({
         }
       }
     },
+    collectCoin() {
+      this.score += this.difficulty * 300;
+    },
+    explosionBomb() {
+      return true;
+    },
+    despawnItems() {
+      for (let item of this.items) {
+        item.timer--;
+        item.timer < 0
+          ? this.items.splice(
+              this.items.findIndex((i) => i == item),
+              1
+            )
+          : null;
+      }
+    },
+    spawnItems() {
+      let type = "";
+      let cords = [] as number[];
+      switch (this.getRandomInt(2)) {
+        case 0:
+          type = "coin";
+          break;
+        case 1:
+          type = "bomb";
+          break;
+      }
+      cords[0] =
+        this.getRandomInt(this.borderRight - this.borderLeft) + this.borderLeft;
+      cords[1] =
+        this.getRandomInt(this.borderDown - this.borderUp) + this.borderUp;
+      this.items.push({
+        type: type,
+        cords: cords,
+        size: "eMedium",
+        timer: 300, //5sek
+      });
+    },
     handlePlayerMovement() {
       if (this.pressedKeys["ArrowDown"]) {
         this.down();
@@ -125,10 +216,31 @@ export default defineComponent({
         this.up();
       }
     },
+    handleEnemyColorSwitch() {
+      for (let enemy of this.Enemies) {
+        if (enemy.type == "colorswitch") {
+          switch (this.getRandomInt(3)) {
+            case 0:
+              enemy.color = "rgb(99, 206, 50)";
+              break;
+            case 1:
+              enemy.color = "rgb(50, 206, 198)";
+              break;
+            case 2:
+              enemy.color = "rgb(84, 50, 206)";
+              break;
+          }
+        }
+      }
+    },
     handleEnemyMovement() {
       for (let enemy of this.Enemies) {
+        if (enemy.type == "curve") {
+          enemy.moveVektor[enemy.moveVektor.findIndex((v) => v != 1)] += 0.02;
+        }
         enemy.x += enemy.moveVektor[0] * this.difficulty;
         enemy.y += enemy.moveVektor[1] * this.difficulty;
+
         if (enemy.y < this.borderUp - 25 || enemy.y > this.borderDown + 25) {
           this.Enemies.splice(
             this.Enemies.findIndex((e) => e == enemy),
@@ -149,6 +261,8 @@ export default defineComponent({
       let size = "";
       let x = 0;
       let y = 0;
+      let type = "";
+      let color = "";
       let moveArray = [] as number[];
       switch (this.getRandomInt(4)) {
         case 0:
@@ -177,12 +291,23 @@ export default defineComponent({
       switch (this.getRandomInt(3)) {
         case 0:
           size = "eSmall";
+          color = "rgb(99, 206, 50)";
           break;
         case 1:
           size = "eMedium";
+          color = "rgb(50, 206, 198)";
           break;
         case 2:
           size = "eBig";
+          color = "rgb(84, 50, 206)";
+          break;
+      }
+      switch (this.getRandomInt(2)) {
+        case 0:
+          type = "curve";
+          break;
+        case 1:
+          type = "colorswitch";
           break;
       }
       this.Enemies[this.Enemies.length] = {
@@ -190,6 +315,8 @@ export default defineComponent({
         y: y,
         size: size,
         id: JSON.stringify(this.getRandomInt(100000000)),
+        type: type,
+        color: color,
         moveVektor: moveArray,
       };
     },
@@ -202,6 +329,7 @@ export default defineComponent({
       this.y = this.borderDown - this.borderUp * 1.5;
       this.x = this.borderRight - this.borderLeft * 2;
       this.Enemies = [] as Enemy[];
+      this.items = [] as Item[];
       this.gameStarted = true;
       window.onkeyup = (e: any) => {
         this.pressedKeys[e.key] = false;
@@ -289,6 +417,20 @@ export default defineComponent({
   width: 25px;
   height: 25px;
   background-color: rgb(84, 50, 206);
+}
+.coin {
+  position: absolute;
+  border-radius: 50%;
+  width: 25px;
+  height: 25px;
+  background-color: rgb(199, 219, 15);
+}
+.bomb {
+  position: absolute;
+  border-radius: 50%;
+  width: 25px;
+  height: 25px;
+  background-color: rgb(0, 0, 0);
 }
 .sideBlock {
   background-color: red;
